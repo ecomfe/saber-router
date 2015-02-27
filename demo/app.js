68414e3b2793da550b41afbe42213ea65ce62aac
treelite <c.xinle@gmail.com>
2015-02-27 13:45:35 +0800

    Update demo

--- demo/app.js
+++ demo/app.js
@@ -3,27 +3,29 @@
  * @author treelite(c.xinle@gmail.com)
  */
 
-define(function () {
+define(function (require) {
 
     var exports = {};
     var content = document.getElementById('content');
+    var extend = require('saber-lang/extend');
 
     document.getElementById('btn').onclick = function () {
         history.back();
     };
 
-    exports.index = function (path, query, url) {
-        console.log(path, query, url);
+    exports.index = function (path, query, params, url) {
+        console.log(path, query, params, url);
         content.innerHTML = 'home page';
     };
 
-    exports.list = function (path, query, url) {
-        console.log(path, query, url);
+    exports.list = function (path, query, params, url) {
+        console.log(path, query, params, url);
         content.innerHTML = 'list page';
     };
 
-    exports.product = function (path, query, url) {
-        console.log(path, query, url);
+    exports.product = function (path, query, params, url) {
+        console.log(path, query, params, url);
+        query = extend(params, query);
         content.innerHTML = 'product: ' + query.id;
     };
 
